c1c666c26bba547e836755ef21fe7b585d4a86c9
Khánh Nguyễn <meikahn0109@gmail.com>
2025-10-01 14:49:11 +0700

    xong giao diện

--- Calculator/app/src/main/java/ntu/anhba/calculator/MainActivity.java
+++ Calculator/app/src/main/java/ntu/anhba/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.anhba.calculator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1, edit2;
+    Button btnCong, btnTru, btnNhan, btnChia;
+    TextView tvKetQua;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        btnCong = findViewById(R.id.btnCong);
+        btnTru = findViewById(R.id.btnTru);
+        btnNhan = findViewById(R.id.btnNhan);
+        btnChia = findViewById(R.id.btnChia);
+        tvKetQua = findViewById(R.id.tvKetqua);
+    }
+    public void HamTinhTong()
+    {
+        
     }
 }

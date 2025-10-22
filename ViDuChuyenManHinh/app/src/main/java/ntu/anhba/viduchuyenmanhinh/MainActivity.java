a48d7b849a6a600569d9e646f440e04fd1b92ae0
Khánh Nguyễn <meikahn0109@gmail.com>
2025-10-22 09:50:47 +0700

    .

--- ViDuChuyenManHinh/app/src/main/java/ntu/anhba/viduchuyenmanhinh/MainActivity.java
+++ ViDuChuyenManHinh/app/src/main/java/ntu/anhba/viduchuyenmanhinh/MainActivity.java
@@ -1,6 +1,11 @@
 package ntu.anhba.viduchuyenmanhinh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +15,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonSend;
+    EditText et_Input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonSend = findViewById(R.id.bt_send);
+        et_Input = findViewById(R.id.et_input);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Code chuyển màn hình
+                //Tạo Intent
+                Intent iChuyen2 = new Intent(MainActivity.this, SecondActivity.class);
+                String data = et_Input.getText().toString();
+                if(data.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Vui Lòng Nhập Name", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    //Gói dữ liệu vào iChuyen2, dùng put`Extra(key,value)
+                    iChuyen2.putExtra("ht", data);
+                    //Kích hoạt màn hình 2
+                    startActivity(iChuyen2);
+                }
 
+            }
+        });
     }
 }
--- ViDuChuyenManHinh/app/src/main/java/ntu/anhba/viduchuyenmanhinh/SecondActivity.java
+++ ViDuChuyenManHinh/app/src/main/java/ntu/anhba/viduchuyenmanhinh/SecondActivity.java
@@ -1,6 +1,10 @@
 package ntu.anhba.viduchuyenmanhinh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +13,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
+    Button bt_Back;
+    TextView tv_Name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        // Nhận về Intent
+        Intent iNhanDuoc = getIntent();
+        // Bóc ra
+        Intent htNhanDuoc = iNhanDuoc.getStringExtra("ht");
+        // Xử lý
+
+
+        //Nút Back
+        bt_Back = findViewById(R.id.bt_back);
+        bt_Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iQuayBack = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(iQuayBack);
+            }
+        });
     }
 }

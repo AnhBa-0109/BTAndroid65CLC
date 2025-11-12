55b0c4aad4576e58a2609da93bf8aeb568feb374
Khánh Nguyễn <meikahn0109@gmail.com>
2025-11-12 14:53:57 +0700

    đã xử lí sự kiện

---
+++ VD_TopAppBar/app/src/main/java/ntu/nguyenquockhanh/vd_topappbar/MainActivity.java
@@ -0,0 +1,39 @@
+package ntu.nguyenquockhanh.vd_topappbar;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private MaterialToolbar topAppBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        topAppBar = findViewById(R.id.topAppBar);
+        // Navigation icon (hamburger/menu)
+        topAppBar.setNavigationOnClickListener(v ->
+                Toast.makeText(this, "Mở menu điều hướng", Toast.LENGTH_SHORT).show()
+        );
+        // Action menu item click
+        topAppBar.setOnMenuItemClickListener(item ->
+        {
+            int itemId = item.getItemId();
+            if (itemId == R.id.action_search) {
+                Toast.makeText(this, "Tìm kiếm", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (itemId == R.id.action_settings) {
+                Toast.makeText(this, "Mở cài đặt", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        });
+    }
+}

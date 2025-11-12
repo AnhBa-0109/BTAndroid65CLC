f279084bf72a040a415c69120a82a619fa10d820
Khánh Nguyễn <meikahn0109@gmail.com>
2025-11-12 13:39:13 +0700

    Xử lí xong sự kiện

--- VD_BottomNavigationView/app/src/main/java/ntu/nguyenquockhanh/vd_bottomnavigationview/MainActivity.java
+++ VD_BottomNavigationView/app/src/main/java/ntu/nguyenquockhanh/vd_bottomnavigationview/MainActivity.java
@@ -1,8 +1,10 @@
 package ntu.nguyenquockhanh.vd_bottomnavigationview;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarItemView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNav;
@@ -19,10 +23,39 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         bottomNav = findViewById(R.id.bottom_nav);
-
+        if (savedInstanceState == null) {
+            LoadFragment(new HomeFragment());
+        }
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment;
+                int itemId = menuItem.getItemId();
+                if(itemId == R.id.nav_home)
+                {
+                    selectedFragment = new HomeFragment();
+                }
+                else if(itemId == R.id.nav_profile)
+                {
+                    selectedFragment = new ProfileFragment();
+                }
+                else if(itemId == R.id.nav_search)
+                {
+                    selectedFragment = new SearchFragment();
+                }
+                else
+                {
+                    return false;
+                }
+                LoadFragment(selectedFragment);
+                return true;
+            }
+        });
     }
     private void LoadFragment(Fragment fragment)
     {
-        
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit();
     }
 }

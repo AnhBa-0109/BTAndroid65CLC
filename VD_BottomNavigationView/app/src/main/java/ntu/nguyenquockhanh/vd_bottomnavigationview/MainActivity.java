b7730508cecce7613ff393e4ada3b3e26f8f4c02
Khánh Nguyễn <meikahn0109@gmail.com>
2025-11-12 13:25:40 +0700

    Tạo xong màn hình Home, Search, Profile và Nav

---
+++ VD_BottomNavigationView/app/src/main/java/ntu/nguyenquockhanh/vd_bottomnavigationview/MainActivity.java
@@ -0,0 +1,28 @@
+package ntu.nguyenquockhanh.vd_bottomnavigationview;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        bottomNav = findViewById(R.id.bottom_nav);
+
+    }
+    private void LoadFragment(Fragment fragment)
+    {
+        
+    }
+}

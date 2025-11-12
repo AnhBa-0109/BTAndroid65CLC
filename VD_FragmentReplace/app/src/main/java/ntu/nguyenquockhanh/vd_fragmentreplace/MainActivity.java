8dfe08df8d560826cc3bb4bb1e1ed2222cc470b7
Khánh Nguyễn <meikahn0109@gmail.com>
2025-11-12 13:06:26 +0700

    Hoàn thành code Replace cho button

--- VD_FragmentReplace/app/src/main/java/ntu/nguyenquockhanh/vd_fragmentreplace/MainActivity.java
+++ VD_FragmentReplace/app/src/main/java/ntu/nguyenquockhanh/vd_fragmentreplace/MainActivity.java
@@ -1,4 +1,4 @@
-package ntu.nguyenquockhanh.vd_fragmentdynamic;
+package ntu.nguyenquockhanh.vd_fragmentreplace;
 
 import android.os.Bundle;
 
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .add(R.id.frameContent, new ContentFragment())
-                .add(R.id.frameFooter, new FooterFragment())
-                .commit();
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
